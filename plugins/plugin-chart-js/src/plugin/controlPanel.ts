26cf160d20d429ead58d78535842f319a4e797ae
igor.sobolevskiy@3dlook.me <igor.sobolevskiy@3dlook.me>
2020-07-29 16:08:37 +0300

    feat: js plugin

--- plugins/plugin-chart-js/src/plugin/controlPanel.ts
+++ plugins/plugin-chart-js/src/plugin/controlPanel.ts
@@ -1,33 +1,14 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+"use strict";
+
 import { t } from '@superset-ui/translation';
 import { ControlPanelConfig } from '@superset-ui/chart-controls';
 
 const config: ControlPanelConfig = {
-
-
-  // For control input types, see: superset-frontend/src/explore/components/controls/index.js
   controlPanelSections: [
     {
       label: t('Query'),
       expanded: true,
-      controlSetRows: [['groupby'], ['metrics'], ['adhoc_filters'], ['row_limit', null]],
+      controlSetRows: [['metrics'], ['adhoc_filters']],
     },
     {
       label: t('Control'),
@@ -49,15 +30,18 @@ const config: ControlPanelConfig = {
       ],
     },
   ],
-  // Time series charts need to override the `druidTimeSeries` and `sqlaTimeSeries`
-  // sections to add the time grain dropdown.
   sectionOverrides: {
-
+    druidTimeSeries: {
+      controlSetRows: [['granularity', 'druid_time_origin'], ['time_range']]
+    },
+    sqlaTimeSeries: {
+      controlSetRows: [['granularity_sqla', 'time_grain_sqla'], ['time_range']]
+    }
   },
   controlOverrides: {
-    row_limit: {
-      default: 100,
-    },
+    y_axis_format: {
+      label: 'Number format'
+    }
   },
 };
 
